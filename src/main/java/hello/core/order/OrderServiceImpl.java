d53b1f677f6eac87028ef242bdc98102ec30a9d8
sangwoo lee <lswoo0705@gmail.com>
2023-10-05 12:04:09 +0900

    스프링 원리 이해2 - 새로운 할인 정책 적용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,14 +1,17 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // DIP, OCP에 위반됨
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // 구체적인 것에 의존하지 않고 인터페이스에만 의존하도록
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
