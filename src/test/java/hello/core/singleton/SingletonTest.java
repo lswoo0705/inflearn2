d545f3510d6e260787c6b91b5ca10485424f9721
sangwoo lee <lswoo0705@gmail.com>
2023-10-10 15:01:45 +0900

    싱글톤 컨테이너

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,11 +18,14 @@ public class SingletonTest {
         // 1. 조회 : 호출 할 때마다 객체 생성
         MemberService memberService1 = appConfig.memberService();
         MemberService memberService2 = appConfig.memberService();
+
         // 두 객체의 참조값이 다른 것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
+
         // memberServise1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
+
         // 요청할 때마다 객체를 새로 생성하기 때문에 메모리의 낭비가 심하다.
         // -> 해당 객체를 하나만 생성하고, 공유하도록 해결 -> 싱글톤 패턴
     }
@@ -35,5 +40,26 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회 : 호출 할 때마다 객체 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 두 객체의 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberServise1 = memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+
+        // 요청 때마다 새로운 객체를 생성하는 것이 아닌, 이미 만들어져있는 객체를 공유하여 효율적으로 재사용이 가능하다.
+        // 하지만, 요청이 들어올 때마다 새로운 객체를 생성하여 반환하는 기능인 '빈 스코프'를 주로 사용한다.
     }
 }
