2cf33c929ee7409fc53accc7ae80bb0e7812a306
sangwoo lee <lswoo0705@gmail.com>
2023-10-17 13:34:02 +0900

    컴포넌트 스캔 의존관계 자동 주입

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,13 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        // 스캔 대상에서 제외
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
